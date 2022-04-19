1e80d77e9404ac974ab877a0cb8011da6a27a5fd
Ales Web <aleswebgit@gmail.com>
2022-04-19 20:14:34 +0200

    Note model, controller, migration and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\Note;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+class DatabaseSeeder extends Seeder {
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run() {
+    User::factory()->create([
+      'email' => 'admin@admin.com',
+      'password' => bcrypt('admin'),
+    ]);
+    Note::factory(30)->create();
+  }
 }
