07016950396a5ad924c26fc53846bc7c5505bbee
Ales Web <aleswebgit@gmail.com>
2022-04-19 21:05:09 +0200

    pass routes with controller PageController, and create routes for notes

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+class PageController extends Controller {
+  public function index() {
+    return Inertia::render('Index', [
+      'canLogin' => Route::has('login'),
+      'canRegister' => Route::has('register'),
+      'laravelVersion' => Application::VERSION,
+      'phpVersion' => PHP_VERSION,
+    ]);
+  }
+  public function dashboard() {
+    return Inertia::render('Dashboard');
+  }
+}
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -11,7 +11,7 @@ defineProps({
 
 <template>
 
-  <Head title="Welcome" />
+  <Head title="Index" />
 
   <div class="relative flex justify-center min-h-screen bg-gray-100 items-top dark:bg-gray-900 sm:items-center sm:pt-0">
     <div v-if="canLogin" class="fixed top-0 right-0 hidden px-6 py-4 sm:block">
